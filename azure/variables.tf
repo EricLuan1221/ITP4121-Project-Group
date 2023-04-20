642ce53dffc6322f31079147fe9a8329b7bf1a05
yumi-or <98079739+yumi-or@users.noreply.github.com>
2023-04-20 15:05:14 +0800

    Update variables.tf

--- azure/variables.tf
+++ azure/variables.tf
@@ -62,9 +62,7 @@ variable "subnets" {
         name="subnet1"
         address_prefixes=["10.0.1.0/24"]
     }
-    subnet_2={
-        name="subnet2"
-        address_prefixes=["10.0.2.0/24"]
+   
     }
   }
 }
